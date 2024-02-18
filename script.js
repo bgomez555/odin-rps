6bbfd7750d7830410f020bfc9140bbcfcb745ef0
bgomez555 <159864723+bgomez555@users.noreply.github.com>
2024-02-18 01:10:37 -0500

    feat: create function determineWinner()

--- script.js
+++ script.js
@@ -2,13 +2,38 @@ function getComputerChoice(){
     let number = Math.floor((Math.random()*3)+1)
     switch(number){
         case 1:
-            return "Rock";
+            return "rock";
             break;
         case 2:
-            return "Paper";
+            return "paper";
             break;
         case 3:
-            return "Scissors";
+            return "scissors";
             break;
     }
 }
+
+function determineWinner(playerSelection, computerSelection){
+    if(playerSelection===computerSelection){
+        return "tie";
+    }
+    else if (playerSelection === "rock" && computerSelection === "paper"){
+        return "computer";
+    }
+    else if (playerSelection === "rock" && computerSelection === "scissors"){
+        return "player";
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock"){
+        return "player";
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors"){
+        return "computer";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock"){
+        return "computer";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper"){
+        return "player";
+    }
+}
+
